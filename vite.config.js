d7706a883db196ae39e7f31f199f738e8c063c56
Toby Lowe <tobyjohnlowe@gmail.com>
2025-11-24 09:16:05 +0000

    new update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/toby_online/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
